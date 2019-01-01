a6528a3c0617bfa63fa8068c3ca0305ba346fda2
CorePoint <CorePoint@users.noreply.github.com>
2019-01-01 01:55:34 +0100

    changed time format to 24h and date format to DD.MM

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,19 +1,19 @@
 function fish_right_prompt -d "Write out the right prompt"
-	set -l last_status $status
+        set -l last_status $status
 
-	if test $last_status -ne 0
-		set_color --bold red
-		echo -ns $last_status ' :( '
-		set_color normal
-	end
+        if test $last_status -ne 0
+                set_color --bold red
+                echo -ns $last_status ' :( '
+                set_color normal
+        end
 
-	# tmux, screen title
-	switch $TERM;
-	case xterm'*' vte'*';
-	printf '\033]0;['(prompt_pwd)']\007';
-	case screen'*';
-	printf '\033k['(prompt_pwd)']\033\\';
-	end
+        # tmux, screen title
+        switch $TERM;
+        case xterm'*' vte'*';
+        printf '\033]0;['(prompt_pwd)']\007';
+        case screen'*';
+        printf '\033k['(prompt_pwd)']\033\\';
+        end
 
-	echo -ns (date "+%D")" - "(date "+%l:%M %p")
+        echo -ns (date "+%d.%m")" - "(date "+%R")
 end
